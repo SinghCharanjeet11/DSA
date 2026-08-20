babb445b241e5520b1dd2d4d01cca9a7877461a3
Charanjeet Singh <charansingh9450v@gmail.com>
2026-08-20 10:33:58 +0530

    Time: 2 ms (57.45%) | Memory: 46.9 MB (12.74%) - LeetSync

---
+++ 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] resultArray(int[] nums) {
+        ArrayList<Integer>al1=new ArrayList<>();
+        ArrayList<Integer>al2=new ArrayList<>();
+
+        int n=nums.length;
+        al1.add(nums[0]);
+        al2.add(nums[1]);
+
+
+        for(int i=2;i<n;i++){
+            if(al1.get(al1.size()-1)>al2.get(al2.size()-1)){
+                al1.add(nums[i]);
+            }
+            else{
+                al2.add(nums[i]);
+            }
+        }
+        al1.addAll(al2);
+        int[] result=new int[n];
+        for(int i=0;i<n;i++){
+            result[i]=al1.get(i);
+        }
+        return result;
+    }
+}
