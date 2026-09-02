7756c62321639e49ae97fd5ab55af318fdd2e802
Charanjeet Singh <charansingh9450v@gmail.com>
2026-09-02 13:15:34 +0530

    Time: 1 ms (31.89%) | Memory: 45.4 MB (16.73%) - LeetSync

--- 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
@@ -1,50 +1,6 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
-        int bitmask = 0;
-        for (int i : nums1) {
-            bitmask ^= i;
-        }
-        int n = nums1.length;
-        int[] nums2 = new int[n];
-        if (bitmask % 2 != 0) {
-            int even = -1;
-            for (int i = 0; i < n; i++) {
-                if (nums1[i] % 2 == 0) {
-                    even = nums1[i];
-                    break;
-                }
-            }
-            if (even != -1) {
-                for (int i = 0; i < n; i++) {
-                    if (nums1[i] % 2 != 0) {
-                        nums2[i] = nums1[i];
-                    } 
-                    else {
-                        nums2[i] = nums1[i] - 1;
-                    }
-                }
-            }
-        } 
-        else {
-            for (int i = 0; i < n; i++) {
-                if (nums1[i] % 2 == 0) {
-                    nums2[i] = nums1[i];
-                } 
-                else {
-                    boolean found = false;
-                    for (int j = 0; j < n; j++) {
-                        if (i != j && nums1[j] % 2 != 0) {
-                            nums2[i] = nums1[i] - nums1[j];
-                            found = true;
-                            break;
-                        }
-                    }
-                    if (!found) {
-                        return false;
-                    }
-                }
-            }
-        }
+        
         return true;
     }
 }
