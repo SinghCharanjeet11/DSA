714a7353ffffd0b3b7673b240a8405e0c8c43c17
Charanjeet Singh <charansingh9450v@gmail.com>
2026-09-14 21:26:33 +0530

    Time: 0 ms (100.00%) | Memory: 48.2 MB (21.59%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,22 +1,17 @@
 class Solution {
-    private static double pow(double x, int n){
+    private static double solve(double x, long n){
         if(n==0){
-            return 1.0;
+            return 1;
         }
-        if(n==1){
-            return x;
+        if (n < 0) {
+            return solve(1 / x, -n);
         }
-        if(n%2==0){
-            return pow(x*x,n/2);
+        if (n % 2 == 0) {
+            return solve(x * x, n / 2);
         }
-        return x*pow(x*x,n/2);
+        return x * solve(x * x, n / 2);
     }
     public double myPow(double x, int n) {
-
-        if(n<0){
-        return 1.0/pow(x,n);
-        }
-        return pow(x,n);
-        
+        return solve(x,(long)n);
     }
 }
